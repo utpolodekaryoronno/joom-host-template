6bdae42cc52b81f8cdd4979d8fc9614ad1621f57
Utpol Odekary Oronno <104867628+utpolodekaryoronno@users.noreply.github.com>
2022-05-20 01:21:56 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+  // owlCarousel
+  // pay-service
+  $('.pay-service').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplay:true,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
+
+
+  $('.people').owlCarousel({
+    loop:true,
+    margin:40,
+    autoplay:true,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+  });
+  // partner
+  $('.partner').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplay:true,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        420:{
+          items:2
+        },
+        575:{
+          items:3
+        },
+        575:{
+            items:4
+        },
+        767:{
+          items:5
+        },
+        1000:{
+            items:6
+        }
+    }
+  });
+
+  // aos
+  // AOS.init();
+  
+});
+
